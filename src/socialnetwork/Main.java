54c4ea8d66b6c440e6197632142770cf2d55b4ba
Discofrog <darabutdaniela@gmail.com>
2021-11-14 21:38:02 +0200

    Buna sunt Daniela si sper ca m-am connectat,acesta este un test

--- src/socialnetwork/Main.java
+++ src/socialnetwork/Main.java
@@ -3,8 +3,11 @@ package socialnetwork;
 import socialnetwork.ui.UserInterface;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("buna sunt Daniela si sper ca m-am conectat ");
         UserInterface ui = new UserInterface();
         ui.run();
+
     }
 }
