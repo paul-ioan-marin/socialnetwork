e7bf983fed3e5cb7c79f37c591e25e3a08fe6193
Discofrog <darabutdaniela@gmail.com>
2021-11-15 21:04:47 +0200

    testam noul branch

--- src/socialnetwork/Main.java
+++ src/socialnetwork/Main.java
@@ -10,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
         ui.run();
+        System.out.println("print");
     }
 }
